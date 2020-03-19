e00fbe714b18bacaec427b84ec2f5303c74be71a
Edimo Silva <edimo.silva@javln.com>
2020-03-19 15:27:16 +1300

    adding missing auth helper

---
+++ volume/tasks-manager/app/helpers/auth/json_web_token_helper.rb
@@ -0,0 +1,55 @@
+module Auth
+  module JsonWebTokenHelper
+    SECRET_KEY = Rails.application.credentials.jwt[:secret_key]
+
+    def encode_user(user, exp = 24.hours.from_now)
+      payload = {}
+      payload[:exp] = exp.to_i
+      payload[:user_id] = user.id
+
+      JWT.encode(payload, SECRET_KEY)
+    end
+
+    def decode_token(token)
+      decoded = JWT.decode(token, SECRET_KEY)[0]
+      HashWithIndifferentAccess.new decoded
+    end
+
+    def header_for_user(user)
+      { 'Authorization' => encode_user(user) }
+    end
+
+    def authorize_request
+      header = request.headers['Authorization']
+      header = header.split(' ').last if header
+      begin
+        @current_user = user_by_token(header)
+      rescue ActiveRecord::RecordNotFound => e
+        render_unauthorized(e.message)
+      rescue JWT::DecodeError
+        render_unauthorized
+      end
+    end
+
+    def public_request
+      header = request.headers['Authorization']
+      return unless header
+
+      header = header.split(' ').last
+      begin
+        @current_user = user_by_token(header)
+      rescue JWT::DecodeError => e
+        render_unauthorized(e.message)
+      end
+    end
+
+    def user_by_token(token)
+      decoded = decode_token(token)
+      User.find(decoded[:user_id])
+    end
+
+    def current_user
+      @current_user
+    end
+  end
+end
