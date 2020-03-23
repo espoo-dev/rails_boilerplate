389568656f0965d47c2c772abef7302d7617fe3b
edimo <edimossilva@gmail.com>
2020-03-24 09:40:55 +1300

    Updating schema

--- volume/tasks-manager/db/migrate/20200303095909_create_users.rb
+++ volume/tasks-manager/db/migrate/20200303095909_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.0]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
+      t.integer :access_level
 
       t.timestamps
     end
--- volume/tasks-manager/db/seeds.rb
+++ volume/tasks-manager/db/seeds.rb
@@ -10,28 +10,3 @@ user1 = User.create(username: 'registered_user1', password: '111', access_level:
 user2 = User.create(username: 'registered_user2', password: '222', access_level: User.access_levels[:registred])
 user3 = User.create(username: 'admin_user1', password: '111', access_level: User.access_levels[:admin])
 user4 = User.create(username: 'admin_user2', password: '222', access_level: User.access_levels[:admin])
-
-blog1 = Blog.create(name: 'registered_user1 news', user_id: user1.id, is_private: false)
-blog2 = Blog.create(name: 'registered_user2 olds', user_id: user2.id, is_private: true)
-blog3 = Blog.create(name: 'registered_user2 olds', user_id: user3.id, is_private: false)
-blog4 = Blog.create(name: 'registered_user2 olds', user_id: user4.id, is_private: true)
-
-Post.create(title: 'registered_user1 blog1 post1', blog_id: blog1.id, user_id: user1.id)
-Post.create(title: 'registered_user2 blog1 post2', blog_id: blog1.id, user_id: user2.id)
-Post.create(title: 'admin_user1 blog1 post3', blog_id: blog1.id, user_id: user3.id)
-Post.create(title: 'admin_user2 blog1 post4', blog_id: blog1.id, user_id: user4.id)
-
-Post.create(title: 'registered_user1 blog2 post1', blog_id: blog2.id, user_id: user1.id)
-Post.create(title: 'registered_user2 blog2 post2', blog_id: blog2.id, user_id: user2.id)
-Post.create(title: 'admin_user1 blog2 post3', blog_id: blog2.id, user_id: user3.id)
-Post.create(title: 'admin_user2 blog2 post4', blog_id: blog2.id, user_id: user4.id)
-
-Post.create(title: 'registered_user1 blog3 post1', blog_id: blog3.id, user_id: user1.id)
-Post.create(title: 'registered_user2 blog3 post2', blog_id: blog3.id, user_id: user2.id)
-Post.create(title: 'admin_user1 blog3 post3', blog_id: blog3.id, user_id: user3.id)
-Post.create(title: 'admin_user2 blog3 post4', blog_id: blog3.id, user_id: user4.id)
-
-Post.create(title: 'registered_user1 blog4 post1', blog_id: blog4.id, user_id: user1.id)
-Post.create(title: 'registered_user2 blog4 post2', blog_id: blog4.id, user_id: user2.id)
-Post.create(title: 'admin_user1 blog4 post3', blog_id: blog4.id, user_id: user3.id)
-Post.create(title: 'admin_user2 blog4 post4', blog_id: blog4.id, user_id: user4.id)
