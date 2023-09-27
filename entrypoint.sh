130dc47ede22971f7118b55ed74dd73cd6b9d6db
Edimo Silva <edimossilva@gmail.com>
2023-09-27 10:04:15 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 cd /app
 sleep 5 # just to make sure postgres is up
 echo "run db:reset"
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:reset db:create db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bin/rails db:reset db:create db:migrate db:seed
 echo "run Puma"
 bin/bundle exec puma -C config/puma.rb
