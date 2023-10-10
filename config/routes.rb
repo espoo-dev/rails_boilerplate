36649de8e1ffe1bd94e258abff2b4a873315a502
Philipe Leandro <philipe-leandro@hotmail.com>
2023-10-10 20:12:41 -0300

    Add v1 to api

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,9 @@ Rails.application.routes.draw do
   get "/search", to: "hello_world#search"
 
   namespace :api, defaults: { format: :json } do
-    get "/public_method", to: "hello_world#public_method"
-    get "/private_method", to: "hello_world#private_method"
+    namespace :v1 do
+      get "/public_method", to: "hello_world#public_method"
+      get "/private_method", to: "hello_world#private_method"
+    end
   end
 end
