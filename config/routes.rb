c3eb5b8a99a27dfb0d26336977bd8f9cac834d45
Philipe Leandro <philipe-leandro@hotmail.com>
2023-10-12 21:28:56 -0300

    Remove comment in line

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 require "sidekiq/web"
 
 Rails.application.routes.draw do
-  # mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
+  mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql" if Rails.env.development?
   post "/graphql", to: "graphql#execute"
   mount Sidekiq::Web => "/sidekiq"
   mount Rswag::Ui::Engine => "/api-docs"
