e3c35ab7e1f51e21d3efc5eb1b71cf13c49b8a2d
Edimo Silva <edimossilva@gmail.com>
2023-10-19 12:45:09 -0300

    Setup strava sign in

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -10,4 +10,18 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
+
+  def strava
+    user = User.find_or_create_from_strava_oauth_provider_data(request.env["omniauth.auth"])
+    if user.persisted?
+      sign_in_and_redirect user
+      set_flash_message(:notice, :success, kind: "Github") if is_navigational_format?
+    else
+      redirect_to new_user_registration_url
+    end
+  end
+
+  def failure
+    redirect_to new_user_registration_url
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :validatable, :api,
-    :omniauthable, omniauth_providers: %i[github google_oauth2]
+    :omniauthable, omniauth_providers: %i[github strava]
 
   validates :email, uniqueness: { case_sensitive: false }
 
@@ -17,6 +17,16 @@ class User < ApplicationRecord
       oauth_uid: oauth_provider_data.uid
     ).first_or_create do |user|
       user.email = oauth_provider_data.info.email
+      user.password = Devise.friendly_token[0, 20]
+    end
+  end
+
+  def self.find_or_create_from_strava_oauth_provider_data(oauth_provider_data)
+    where(
+      oauth_provider: oauth_provider_data.provider,
+      oauth_uid: oauth_provider_data.uid
+    ).first_or_create do |user|
+      user.email = "#{oauth_provider_data.uid}@strava_unknown_email.com"
       user.password = Devise.friendly_token[0, 20]
     end
   end
