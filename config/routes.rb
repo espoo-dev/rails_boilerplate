a08dac4dfcbf2e1a6ae6bc21b3c7dd70326d328d
Edimo Silva <edimossilva@gmail.com>
2023-11-01 19:45:36 -0300

    161: move hello world related code to demo pack

--- config/routes.rb
+++ config/routes.rb
@@ -8,21 +8,18 @@ Rails.application.routes.draw do
   mount Sidekiq::Web => "/sidekiq"
   mount Rswag::Ui::Engine => "/api-docs"
   mount Rswag::Api::Engine => "/api-docs"
+
   devise_for :users, controllers: { omniauth_callbacks: "oauth/controllers/omniauth_callbacks" }
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  get "/public_method", to: "hello_world#public_method"
-  get "/private_method", to: "hello_world#private_method"
+  get "/public_method", to: "demo_pack/controllers/hello_world#public_method"
+  get "/private_method", to: "demo_pack/controllers/hello_world#private_method"
   get "/search", to: "hello_world#search"
+
+  get "api/v1/public_method", to: "demo_pack/controllers/api/v1/hello_world#public_method"
+  get "api/v1/private_method", to: "demo_pack/controllers/api/v1/hello_world#private_method"
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      get "/public_method", to: "hello_world#public_method"
-      get "/private_method", to: "hello_world#private_method"
-
       resources :users, only: [:index]
     end
   end
