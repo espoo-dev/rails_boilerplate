c72fa7522c0799a7ed091fa4f26809535ae5ef08
Daniel Nottingham <danielnottingham.ti@gmail.com>
2023-11-03 11:38:28 -0300

    fix: remove credentials true

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -3,6 +3,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"
-    resource "*", headers: :any, methods: %i[get post put patch delete options head], credentials: true
+    resource "*", headers: :any, methods: %i[get post put patch delete options head]
   end
 end
