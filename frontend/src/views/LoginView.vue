d9f0e31fad8a80a814894e7faf665aeae9e751de
Edimo Silva <edimossilva@gmail.com>
2024-05-24 17:21:57 -0300

    182: create login view page

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,40 @@
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    // state
+    const username = ref("user@email.com")
+    const password = ref("password")
+
+    // methods
+    const doLogin = () => {
+      console.log(`${username.value} , ${password.value}`)
+    }
+
+    return {
+      username,
+      password,
+      doLogin
+    }
+  },
+}
+</script>
+
+<template>
+  <div class="login">
+    <h2>Login</h2>
+    <form @submit.prevent="">
+      <div>
+        <label for="username">Username:</label>
+        <input type="text" id="username" v-model="username" required />
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input type="password" id="password" v-model="password" required />
+      </div>
+      <button @click="doLogin">Login</button>
+    </form>
+  </div>
+</template>
