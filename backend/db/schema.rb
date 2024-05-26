bb774eddb23ed2de72bbdfbea09a4974d4fdd2cb
Edimo Silva <edimossilva@gmail.com>
2024-05-26 10:21:02 -0300

    188: setup act as paranoid to all classes (#192)

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_25_174254) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_26_124316) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "citext"
   enable_extension "plpgsql"
@@ -117,6 +117,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_25_174254) do
     t.string "oauth_provider"
     t.string "oauth_uid"
     t.boolean "admin", default: false, null: false
+    t.datetime "deleted_at"
+    t.index ["deleted_at"], name: "index_users_on_deleted_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
