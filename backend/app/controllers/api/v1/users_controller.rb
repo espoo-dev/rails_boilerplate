c0084b4408fd0fa88eaaad93907da8dcdf865521
Edimo Silva <edimossilva@gmail.com>
2024-05-30 09:38:35 -0300

    196 add endpoint to create user (#198)
    * 196: use dry-schemas on users controller
    
    * 196: fix typo
    
    * 196: add endpoint to create user
    
    * 196: refactor UserContracts
    
    * 196: refactor contracts
    
    * 196: remove unused files
    
    * 196: fix cops

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -2,7 +2,9 @@
 
 module Api
   module V1
-    class UsersController < ApiController
+    class UsersController < Api::V1::ApiController
+      skip_before_action :authenticate_devise_api_token!, only: :create
+
       def index
         users = User.page(page).per(per_page)
 
@@ -11,14 +13,32 @@ module Api
         render json: users, status: :ok
       end
 
+      def create
+        user = User.new(user_create_contract)
+
+        authorize(user)
+
+        user.save!
+
+        render json: user, status: :created
+      end
+
       private
 
+      def user_index_contract
+        @user_index_contract ||= UserContracts::Index.call(permitted_params(:page, :per_page))
+      end
+
+      def user_create_contract
+        UserContracts::Create.call(permitted_params(:email, :password))
+      end
+
       def page
-        params[:page]
+        user_index_contract[:page]
       end
 
       def per_page
-        params[:per_page]
+        user_index_contract[:per_page]
       end
     end
   end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
 
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :users, only: [:index]
+      resources :users, only: %i[index create]
     end
   end
 end
