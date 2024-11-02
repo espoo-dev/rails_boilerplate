1a9978b932b49c94f7755028a6572787f0cf5813
valmirosjunior <valmirosjunior@gmail.com>
2024-11-01 21:51:34 -0300

    215- add shared context for when user is not authenticated

--- backend/spec/requests/api/v1/hello_word_request_spec.rb
+++ backend/spec/requests/api/v1/hello_word_request_spec.rb
@@ -18,19 +18,7 @@ RSpec.describe "HelloWorlds" do
   describe "GET /api/v1/private_method" do
     let(:user) { create(:user) }
 
-    context "when user is not authenticated" do
-      before do
-        get "/api/v1/private_method"
-      end
-
-      it "renders json with message" do
-        expect(response.body).to include("error", "Invalid token")
-      end
-
-      it "receives http status unauthorized" do
-        expect(response).to have_http_status(:unauthorized)
-      end
-    end
+    include_context "when user is not authenticated" 
 
     context "when user is authenticated" do
       let(:instance_token) { Devise::Api::TokensService::Create.new(resource_owner: user, previous_refresh_token: nil) }
