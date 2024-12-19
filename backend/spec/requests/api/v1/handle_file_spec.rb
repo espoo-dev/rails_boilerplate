fa2b75e4dbb8f8035fa2b3c6f1662b16ac86f527
Lucas Leandro <lukkassoliveira2215@gmail.com>
2024-12-18 21:44:02 -0300

    tests csv

--- backend/spec/requests/api/v1/handle_file_spec.rb
+++ backend/spec/requests/api/v1/handle_file_spec.rb
@@ -7,15 +7,11 @@ RSpec.describe "HandleFiles" do
 
       it 'returns status ok' do
         post '/api/v1/import', params: { file: csv_file }
-
-
         expect(response).to have_http_status(:ok)
       end
 
       it 'returns the correct JSON response with file rows' do
         post '/api/v1/import', params: { file: csv_file }
-
-
         expect(JSON.parse(response.body)['file_rows']).to be_a(Hash)
       end
     end
@@ -23,33 +19,29 @@ RSpec.describe "HandleFiles" do
 
     context 'when CSV file is empty' do
       let(:empty_csv_file) { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'empty_file.csv'), 'text/csv') }
-    
+  
       it 'returns status ok' do
         post '/api/v1/import', params: { file: empty_csv_file }
-    
         expect(response).to have_http_status(:ok)
       end
     
       it 'returns empty file rows in JSON' do
         post '/api/v1/import', params: { file: empty_csv_file }
-    
         expect(JSON.parse(response.body)['file_rows']).to be_empty
       end
     end
 
-    context 'when file is not a CSV' do
-      let(:non_csv_file) { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'non_csv_file.txt'), 'text/csv') }
-    
-      it 'returns an error status' do
-        post '/api/v1/import', params: { file: non_csv_file }
-    
-        expect(response).to have_http_status(:unprocessable_entity)
+    context "when CSV file is valid" do
+      let(:csv_file) { fixture_file_upload(Rails.root.join('spec', 'fixtures', 'files', 'valid_file.csv'), 'text/csv') }
+
+      it "returns status ok" do
+        post '/api/v1/import', params: { file: csv_file }
+        expect(response).to have_http_status(:ok)
       end
-    
-      it 'returns an error message' do
-        post '/api/v1/import', params: { file: non_csv_file }
-    
-        expect(response.body).to include('Invalid file type')
+
+      it "returns the correct JSON response with file rows" do
+        post '/api/v1/import', params: { file: csv_file }
+        expect(response.body).to include('file_rows')
       end
     end
   end
